a8fdc83ed61a51ef3ea50ef863026bc83797c055
jaeshim <jaeshim@gmarket.com>
2022-10-27 11:10:40 +0900

    스크립트 수정

--- Shell-Script/Kafka/delete_old_topics.sh
+++ Shell-Script/Kafka/delete_old_topics.sh
@@ -1,6 +1,6 @@
 #30일전 이름을 가진 토픽들을 배열에 담음 
-YESTERDAY=`date +%Y%m%d -d -30day`
-TOPICS=(`sudo kafka-topics --bootstrap-server localhost:9092 --list | grep $YESTERDAY`)
+OLDDAY=`date +%Y%m%d -d -30day`
+TOPICS=(`sudo kafka-topics --bootstrap-server localhost:9092 --list | grep $OLDDAY`)
 
 #배열을 순회하면서 삭제 
 for topic in ${TOPICS[@]}
